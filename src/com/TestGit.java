62936d3146175443d9cb6f6bab29808242790ec1
hxp <hxp@hxpgit.com>
2023-11-18 14:03:22 +0800

    hot-fix first commit

--- src/com/TestGit.java
+++ src/com/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("你好   1");
         System.out.println("你好   2");
+        System.out.println("你好   hot-fix");
     }
 }
